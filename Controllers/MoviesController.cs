68b94474bd7dd7cccae30ad558f99df1e7bbbcc9
Benedict Nguyen <benedicn@gmail.com>
2020-04-15 22:51:22 -0500

    update some progress

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            return View(movie);
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2})}")]
+
+        public ActionResult ByReleaseYear(int year, int month)
+        {
+            return Content( year + "/" + month);
+        }
+    }
+}
